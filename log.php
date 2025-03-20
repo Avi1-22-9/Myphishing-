830b390090372cfaffa60defbe1b77c0cd32e2fa
MEDI AVINASH <mediavinash107@gmail.com>
2025-03-20 11:01:03 +0530

    Update log.php

--- log.php
+++ log.php
@@ -1,1 +1,33 @@
+<?php
 
+// Get visitor details
+
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+$timestamp = date("Y-m-d H:i:s");
+
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+
+
+// Detect if the device is Android
+
+$is_android = (stripos($user_agent, "Android") !== false) ? "Yes" : "No";
+
+
+
+// Log the details to a file
+
+$log_entry = "IP: $ip_address | Time: $timestamp | Android: $is_android | User-Agent: $user_agent\n";
+
+file_put_contents("ip_log.txt", $log_entry, FILE_APPEND);
+
+
+
+// Redirect to a fake success page
+
+header("Location: success.html");
+
+exit();
+
+?>
